448577ca8c2942328753d3fcdcea282d7317fd8e
손해원 <sonhw3838@google.com>
2023-01-28 20:30:00 +0900

    first commit

---
+++ js/skilldate.js
@@ -0,0 +1,135 @@
+const all = [
+  {
+    name: 'HTML5',
+    src: './js/images/skill1.jpg',
+    info01: '웹 사이트의 구조를 구성 및 개발 가능',
+    info02: '시멘틱 마크업을 적재적소에 사용 가능'
+  },
+  {
+    name: 'CSS3',
+    src: './js/images/skill2.jpg',
+    info01: 'HTML과 연동하여 레이아웃 디자인 구현 가능',
+    info02: '웹페이지의 다양한 구성을 이해하여 디자인에 맡게 제작하거나 수정할 수 있음'
+  },
+  {
+    name: 'JavaScript',
+    src: './js/images/skill3.jpg',
+    info01: '함수를 이용한해 화면 구현, JS를 이용한 DOM 조작 가능',
+    info02: '콘텐츠에 동적 기능 부여, 멀티미디어 제어, 애니메이션 추가 등 다양한 기능 활용하여 웹 사이트 제작 가능'
+  },
+  {
+    name: 'jQuery',
+    src: './js/images/skill4.jpg',
+    info01: '자바스크립트로 만들어진 함수를 호출, 사용해 기능 구현 가능',
+    info02: '라이브러리를 활용하여 다양한 이벤트 제어 가능'
+  },
+  {
+    name:'React',
+    src: './js/images/skill5.jpg',
+    info01: 'tyled 컴포넌트 적용 가능',
+    info02: '추가 예정입니다.'
+  },
+  {
+    name:'SASS',
+    src: './js/images/skill6.jpg',
+    info01: 'mixin과 include를 활용하여 작성가능',
+    info02: '추가 예정입니다.'
+  },
+  {
+    name:'Node.js',
+    src: './js/images/skill7.jpg',
+    info01: '추가 예정입니다.',
+    info02: '추가 예정입니다.'
+  },
+  {
+    name:'Git',
+    src: './js/images/skill8.jpg',
+    info01: 'push , pull , commit 을 활용하여 형상관리 가능',
+    info02: 'branch를 나누어 통합 작업 가능'
+  },
+  {
+    name:'GitHub',
+    src: './js/images/skill9.jpg',
+    info01: 'git을 통한 형상관리와 이를 github에 연결하여 배포 가능',
+    info02: '추가 예정입니다.'
+  },
+  {
+    name:'AdobeXd',
+    src: './js/images/skill10.jpg',
+    info01: 'XD를 활용하여 시안 제작 가능',
+    info02: '추가 예정입니다.'
+  }
+] 
+
+
+const language = [
+  {
+    name: 'HTML5',
+    src: './js/images/skill1.jpg',
+    info01: '웹 사이트의 구조를 구성 및 개발 가능',
+    info02: '시멘틱 마크업을 적재적소에 사용 가능'
+  },
+  {
+    name: 'CSS3',
+    src: './js/images/skill2.jpg',
+    info01: 'HTML과 연동하여 레이아웃 디자인 구현 가능',
+    info02: '웹페이지의 다양한 구성을 이해하여 디자인에 맡게 제작하거나 수정할 수 있음'
+  },
+  {
+    name: 'JavaScript',
+    src: './js/images/skill3.jpg',
+    info01: '함수를 이용한해 화면 구현, JS를 이용한 DOM 조작 가능',
+    info02: '콘텐츠에 동적 기능 부여, 멀티미디어 제어, 애니메이션 추가 등 다양한 기능 활용하여 웹 사이트 제작 가능'
+  },
+  {
+    name: 'jQuery',
+    src: './js/images/skill4.jpg',
+    info01: '자바스크립트로 만들어진 함수를 호출, 사용해 기능 구현 가능',
+    info02: '라이브러리를 활용하여 다양한 이벤트 제어 가능'
+  }
+]
+
+
+const frontEnd = [
+  {
+    name:'React',
+    src: './js/images/skill5.jpg',
+    info01: 'tyled 컴포넌트 적용 가능',
+    info02: '추가 예정입니다.'
+  },
+  {
+    name:'SASS',
+    src: './js/images/skill6.jpg',
+    info01: 'mixin과 include를 활용하여 작성가능',
+    info02: '추가 예정입니다.'
+  }
+]
+
+const etc = [
+  {
+    name:'Node.js',
+    src: './js/images/skill7.jpg',
+    info01: '추가 예정입니다.',
+    info02: '추가 예정입니다.'
+  },
+  {
+    name:'Git',
+    src: './js/images/skill8.jpg',
+    info01: 'push , pull , commit 을 활용하여 형상관리 가능',
+    info02: 'branch를 나누어 통합 작업 가능'
+  },
+  {
+    name:'GitHub',
+    src: './js/images/skill9.jpg',
+    info01: 'git을 통한 형상관리와 이를 github에 연결하여 배포 가능',
+    info02: '추가 예정입니다.'
+  },
+  {
+    name:'AdobeXd',
+    src: './js/images/skill10.jpg',
+    info01: 'XD를 활용하여 시안 제작 가능',
+    info02: '추가 예정입니다.'
+  }
+]
+
+export {all,language,frontEnd,etc}
